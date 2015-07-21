8c741530f66db9304a84ec855e6d1be3dabbed5d
iamzken <1965571954@qq.com>
2015-07-21 13:27:21 +0800

    发布webservice需要执行的命令和环境
    发布webservice需要执行的命令和环境

---
+++ webservice01/发布webservice需要执行的命令和环境/webservice/deploywebservice.bat
@@ -0,0 +1,3 @@
+java -cp axis-ant.jar;axis-schema.jar;axis.jar;commons-discovery-0.2.jar;commons-logging-1.0.4.jar;jaxrpc.jar;log4j-1.2.8.jar;saaj.jar;wsdl4j-1.5.1.jar;xmlsec-1.3.0.jar;mailapi.jar org.apache.axis.client.AdminClient -lhttp://localhost:8888/webservice06/services/AdminService deploy.wsdd
+
+pause
